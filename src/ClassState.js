b4cafce2589aec0ec00c6dc3acc70625a1e5c79f
Cristina Hernández <achh17@gmail.com>
2023-03-09 18:03:49 -0500

    I added the cycle life in React.Component I created a new component Loading.js

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,39 @@
 import React from "react";
-
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             error: false,
+            loading: false,
+        };
+    }
+
+    UNSAFE_componentWillMount() {
+        console.log("UNSAFE_componentWillMount");
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+
+    componentDidUpdate() {
+        console.log("Actualizando");
+
+        if (!!this.state.loading) {
+            setTimeout(() => {
+                console.log("Haciendo la validacion");
+
+                this.setState({ loading: false });
+
+                console.log("Finalizando la validacion");
+            }, 3000);
         }
     }
+
+
+
 
     render() {
         return (
@@ -18,15 +44,21 @@ class ClassState extends React.Component {
                 {this.state.error && (
                     <p>El código es es incorrecto</p>
                 )}
+                {this.state.loading && (
+                    <Loading />
+                )}
+
 
                 <input type='text' placeholder='código de seguridad' />
                 <button
                     // onClick={()=>this.setState({ error: !this.state.error})} // otra forma de escribirlo
-                    onClick={() => this.setState(prevState => ({ error: !prevState.error }))}
+                    //onClick={() => this.setState(prevState => ({ error: !prevState.error }))}
+                    onClick={() => this.setState({ loading: true })}
                 >Comprobar</button>
             </div>
         );
     }
 }
+
 
 export { ClassState }
